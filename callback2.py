194e263f91740d4fe8e33730f4b69db02ec06739
dolivmorais <dolivmorais@gmail.com>
2024-05-15 21:36:52 -0300

    atualizacao estudos em callbaks

---
+++ callback2.py
@@ -0,0 +1,40 @@
+import dash
+from dash import html, dcc
+import plotly.express as px
+from dash.dependencies import Input, Output, State
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv")
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        step=None
+    )
+])
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtred_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtred_df, x="gdpPercap", y="lifeExp",
+                     size ="pop", color="continent",hover_name="country",
+                     log_x=True,size_max=55)
+    
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ callback3.py
@@ -0,0 +1,79 @@
+import dash
+from dash import html, dcc
+import plotly.express as px
+from dash.dependencies import Input, Output, State
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = pd.read_csv("https://plotly.github.io/datasets/country_indicators.csv")
+
+available_indicators = df['Indicator Name'].unique()
+
+app.layout = html.Div([
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Fertility rate, total (births per woman)'
+            ),
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+            style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Life expectancy at birth, total (years)'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ], style={'width': '48%', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        id='year--slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()},
+        step=None
+    )
+])
+
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    [Input('xaxis-column', 'value'),
+     Input('yaxis-column', 'value'),
+     Input('xaxis-type', 'value'),
+     Input('yaxis-type', 'value'),
+     Input('year--slider', 'value')])
+def update_graph(xaxis_column_name, yaxis_column_name, xaxis_type, yaxis_type, year_value):
+    dff = df[df['Year'] == year_value]
+
+    fig = px.scatter(x=dff[xaxis_column_name],
+                     y=dff[yaxis_column_name],
+                     hover_name=dff['Country Name'])
+
+    fig.update_layout(xaxis={'type': 'linear' if xaxis_type == 'Linear' else 'log'},
+                      yaxis={'type': 'linear' if yaxis_type == 'Linear' else 'log'})
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
